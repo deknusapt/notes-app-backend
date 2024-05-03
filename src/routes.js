13d9360d3368b959c4723a3b41b1a05ba91610c2
deknusapt <dev.deknusa@gmail.com>
2024-05-03 15:49:36 +0800

    first commit project

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const { addNoteHandler, getAllNotesHandler, getNotesByIdHandler } = require("./handler");
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNotesByIdHandler,
+  },
+];
+
+module.exports = routes;
