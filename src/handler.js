220bbc577e15a03bd993d3915e4216eac40eaec7
deknusapt <dev.deknusa@gmail.com>
2024-05-03 16:25:20 +0800

    adding PUT and DELETE routes

--- src/handler.js
+++ src/handler.js
@@ -64,4 +64,56 @@ const getNotesByIdHandler = (request, h) => {
     return response;
 };
 
-module.exports = {addNoteHandler, getAllNotesHandler, getNotesByIdHandler};
+const editNotesByIdHandler = (request, h) => {
+    const {id} = request.params;
+
+    const {title, tags, body} = request.payload;
+    const updatedAt = new Date().toISOString;
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index != -1) {
+      notes[index] ={
+        ...notes[index], title, tags, body, updatedAt,
+      };
+
+      const response = h.response({
+        status: 'success',
+        message: 'Catatan berhasil diperbarui',
+      });
+      response.code(200);
+      return response;
+    }
+
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbaharui catatan. ID tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+
+const deleteNotesByIdHandler = (request, h) => {
+    const {id} = request.params;
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+      notes.splice(index, 1);
+      const response = h.response({
+        status: 'success',
+        message: 'Catatan berhasil dihapus',
+      });
+      response.code(200);
+      return response;
+    }
+
+    const response = h.response({
+      status: 'fail',
+      message: 'Catatan gagal dihapus. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+
+module.exports = {addNoteHandler, getAllNotesHandler, getNotesByIdHandler, editNotesByIdHandler, deleteNotesByIdHandler};
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { addNoteHandler, getAllNotesHandler, getNotesByIdHandler } = require("./handler");
+const { addNoteHandler, getAllNotesHandler, getNotesByIdHandler, editNotesByIdHandler, deleteNotesByIdHandler } = require("./handler");
 
 const routes = [
   {
@@ -15,6 +15,16 @@ const routes = [
     method: 'GET',
     path: '/notes/{id}',
     handler: getNotesByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNotesByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNotesByIdHandler,
   },
 ];
 
